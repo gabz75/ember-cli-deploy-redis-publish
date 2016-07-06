6e258e91d26f5d646c187a75bf1eb2e1cc446896
gabe <gabriel.debeaupuis@gmail.com>
2016-07-06 13:52:04 -0700

    cleanup

--- tests/unit/index-nodetest.js
+++ tests/unit/index-nodetest.js
@@ -2,47 +2,53 @@
 /* jshint jasmine: true */
 'use strict';
 
-var assert = require('../helpers/assert');
-var chai = require('chai');
+const chai           = require('chai');
+const chaiAsPromised = require('chai-as-promised');
 
-var stubProject = {
-  name: function(){
+chai.use(chaiAsPromised);
+
+const assert = chai.assert;
+
+const stubProject = {
+  name() {
     return 'my-project';
-  }
+  },
 };
 
-describe('ember-cli-deploy-redis-publish plugin', function() {
-  var subject, mockUi;
+describe('ember-cli-deploy-redis-publish plugin', () => {
+  let subject;
+  let mockUi;
 
-  beforeEach(function() {
+  beforeEach(() => {
     subject = require('../../index');
     mockUi = {
       verbose: true,
       messages: [],
-      write: function() { },
-      writeLine: function(message) {
+      write() { },
+
+      writeLine(message) {
         this.messages.push(message);
-      }
+      },
     };
   });
 
-  it('implements the didActivate', function() {
-    var plugin = subject.createDeployPlugin({
-      name: 'mock-plugin'
+  it('implements the didActivate', () => {
+    let plugin = subject.createDeployPlugin({
+      name: 'mock-plugin',
     });
     assert.ok(plugin.didActivate);
   });
 
-  describe('configure hook', function() {
-    it('throw an error if redis is missing', function() {
-      var plugin = subject.createDeployPlugin({
-        name: 'ember-cli-deploy-redis-publish'
+  describe('configure hook', () => {
+    it('throw an error if redis is missing', () => {
+      let plugin = subject.createDeployPlugin({
+        name: 'ember-cli-deploy-redis-publish',
       });
 
-      var context = {
+      let context = {
         ui: mockUi,
         project: stubProject,
-        config: {}
+        config: {},
       };
 
       plugin.beforeHook(context);
@@ -52,17 +58,17 @@ describe('ember-cli-deploy-redis-publish plugin', function() {
       }).to.throw('Missing configuration for redis.host');
     });
 
-    it('throw an error if redis.host is missing', function() {
-      var plugin = subject.createDeployPlugin({
-        name: 'ember-cli-deploy-redis-publish'
+    it('throw an error if redis.host is missing', () => {
+      let plugin = subject.createDeployPlugin({
+        name: 'ember-cli-deploy-redis-publish',
       });
 
-      var context = {
+      let context = {
         ui: mockUi,
         project: stubProject,
         config: {
-          redis: {}
-        }
+          redis: {},
+        },
       };
 
       plugin.beforeHook(context);
@@ -72,19 +78,19 @@ describe('ember-cli-deploy-redis-publish plugin', function() {
       }).to.throw('Missing configuration for redis.host');
     });
 
-    it('throw an error if redis.port is missing', function() {
-      var plugin = subject.createDeployPlugin({
-        name: 'ember-cli-deploy-redis-publish'
+    it('throw an error if redis.port is missing', () => {
+      let plugin = subject.createDeployPlugin({
+        name: 'ember-cli-deploy-redis-publish',
       });
 
-      var context = {
+      let context = {
         ui: mockUi,
         project: stubProject,
         config: {
           redis: {
             host: '127.0.0.1',
-          }
-        }
+          },
+        },
       };
 
       plugin.beforeHook(context);
@@ -94,20 +100,20 @@ describe('ember-cli-deploy-redis-publish plugin', function() {
       }).to.throw('Missing configuration for redis.port');
     });
 
-    it('throw an error if redis.password is missing', function() {
-      var plugin = subject.createDeployPlugin({
-        name: 'ember-cli-deploy-redis-publish'
+    it('throw an error if redis.password is missing', () => {
+      let plugin = subject.createDeployPlugin({
+        name: 'ember-cli-deploy-redis-publish',
       });
 
-      var context = {
+      let context = {
         ui: mockUi,
         project: stubProject,
         config: {
           redis: {
             host: '127.0.0.1',
             port: 6379,
-          }
-        }
+          },
+        },
       };
 
       plugin.beforeHook(context);
@@ -116,99 +122,5 @@ describe('ember-cli-deploy-redis-publish plugin', function() {
         plugin.didActivate(context);
       }).to.throw('Missing configuration for redis.password');
     });
-
-    it('throw an error if redisPublish is missing', function() {
-      var plugin = subject.createDeployPlugin({
-        name: 'ember-cli-deploy-redis-publish'
-      });
-
-      var context = {
-        ui: mockUi,
-        project: stubProject,
-        config: {
-          redis: {
-            host: '127.0.0.1',
-            port: 6379,
-            password: 'foobar'
-          }
-        }
-      };
-
-      plugin.beforeHook(context);
-      plugin.configure(context);
-      chai.expect(() => {
-        plugin.didActivate(context);
-      }).to.throw('Missing configuration for redisPublish');
-    });
-
-    // it('throw an error if one redisPublish object does not have a channel', function() {
-    //   var plugin = subject.createDeployPlugin({
-    //     name: 'ember-cli-deploy-redis-publish'
-    //   });
-
-    //   var context = {
-    //     ui: mockUi,
-    //     project: stubProject,
-    //     config: {
-    //       redis: {
-    //         host: '127.0.0.1',
-    //         port: 6379,
-    //         password: 'foobar'
-    //       },
-    //       redisPublish: [
-    //         {
-    //           message: 'bar',
-    //         }
-    //       ]
-    //     },
-    //     _redisLib: new FakeRedisLib(),
-    //   };
-
-    //   plugin.beforeHook(context);
-    //   plugin.configure(context);
-    //   chai.expect(() => {
-    //     plugin.didActivate(context);
-    //   }).to.throw('invalid configuration for redisPublish');
-    // });
   });
-
-  // describe('didActivate hook', function() {
-  //   it('prints messages about published events', function() {
-  //     // var messageOutput = '';
-
-  //     // var plugin = subject.createDeployPlugin({
-  //     //   name: 'redis'
-  //     // });
-  //     // plugin.upload = function(){};
-  //     // plugin.activate = function(){};
-
-  //     // var context = {
-  //     //   deployTarget: 'qa',
-  //     //   ui: {
-  //     //     write: function(message){
-  //     //       messageOutput = messageOutput + message;
-  //     //     },
-  //     //     writeLine: function(message){
-  //     //       messageOutput = messageOutput + message + '\n';
-  //     //     }
-  //     //   },
-  //     //   project: stubProject,
-  //     //   config: {
-  //     //     redis: { }
-  //     //   },
-  //     //   revisionData: {
-  //     //     revisionKey: '123abc',
-  //     //   }
-  //     // };
-  //     // plugin.beforeHook(context);
-  //     // plugin.configure(context);
-  //     // plugin.beforeHook(context);
-  //     // plugin.didDeploy(context);
-  //     // assert.match(messageOutput, /Deployed but did not activate revision 123abc./);
-  //     // assert.match(messageOutput, /To activate, run/);
-  //     // assert.match(messageOutput, /ember deploy:activate qa --revision=123abc/);
-
-  //     assert.ok(true);
-  //   });
-  // });
 });
